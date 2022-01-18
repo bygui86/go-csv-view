e9a8fef6d68a1996a6a24aba797b6b70893c4952
Matteo Baiguini <mbaiguini@swissblock.net>
2022-01-18 20:11:48 +0100

    Added alaingilbert-go-echarts

---
+++ alaingilbert-go-echarts/charts/scatter.go
@@ -0,0 +1,44 @@
+package charts
+
+import (
+	"github.com/bygui86/go-csv-view/alaingilbert-go-echarts/opts"
+	"github.com/bygui86/go-csv-view/alaingilbert-go-echarts/render"
+	"github.com/bygui86/go-csv-view/alaingilbert-go-echarts/types"
+)
+
+// Scatter represents a scatter chart.
+type Scatter struct {
+	RectChart
+}
+
+// Type returns the chart type.
+func (Scatter) Type() string { return types.ChartScatter }
+
+// NewScatter creates a new scatter chart.
+func NewScatter() *Scatter {
+	c := &Scatter{}
+	c.initBaseConfiguration()
+	c.Renderer = render.NewChartRender(c, c.Validate)
+	c.hasXYAxis = true
+	return c
+}
+
+// SetXAxis adds the X axis.
+func (c *Scatter) SetXAxis(x interface{}) *Scatter {
+	c.xAxisData = x
+	return c
+}
+
+// AddSeries adds the new series.
+func (c *Scatter) AddSeries(name string, data []opts.ScatterData, options ...SeriesOpts) *Scatter {
+	series := SingleSeries{Name: name, Type: types.ChartScatter, Data: data}
+	series.configureSeriesOpts(options...)
+	c.MultiSeries = append(c.MultiSeries, series)
+	return c
+}
+
+// Validate validates the given configuration.
+func (c *Scatter) Validate() {
+	c.XAxisList[0].Data = c.xAxisData
+	c.Assets.Validate(c.AssetsHost)
+}
